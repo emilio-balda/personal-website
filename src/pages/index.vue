bf43d668277b9fec1df4aaeb19e40129102551a4
Emilio Balda <me@emilio-balda.com>
2022-03-21 22:46:00 +0100

    feat: add stying to landing page button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,23 +3,35 @@ import TableResponsive from '~/components/TableResponsive.vue'
 </script>
 
 <template>
-  <h1 class="text-8xl italic py-10 px-10">Some awesome quote</h1>
+  <h1 class="text-8xl italic py-10 px-10">
+    Some awesome quote
+  </h1>
   <table-responsive>
     <tr>
       <td class="px-4">
-        <img src="../assets/me.png" alt="Avatar" class="w-96 rounded-full" />
+        <img src="../assets/me.png" alt="Avatar" class="w-96 rounded-full">
       </td>
       <td class="px-4">
-        <h4 class="text-3xl text-left font-bold whitespace-nowrap">Emilio Balda</h4>
-        <p class="text-3xl text-left whitespace-nowrap">Fullstack Developer</p>
-        <p class="text-3xl text-left whitespace-nowrap">PhD in Machine Learning</p>
-        <p class="text-3xl text-left whitespace-nowrap"><a href="mailto:me@emilio-balda.com">me@emilio-balda.com</a></p>
+        <h4 class="text-3xl text-left font-bold whitespace-nowrap">
+          Emilio Balda
+        </h4>
+        <p class="text-3xl text-left whitespace-nowrap">
+          Fullstack Developer
+        </p>
+        <p class="text-3xl text-left whitespace-nowrap">
+          PhD in Machine Learning
+        </p>
+        <p class="text-3xl text-left whitespace-nowrap">
+          <a href="mailto:me@emilio-balda.com">me@emilio-balda.com</a>
+        </p>
       </td>
       <td class="px-16">
         <p class="text-2xl text-justify py-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non magna id risus rutrum pulvinar quis in libero.
         </p>
-        <button class="button-64" role="button"><span class="text">Get Started</span></button>
+        <button class="bn5" role="button">
+          <span class="text">Get Started</span>
+        </button>
       </td>
     </tr>
   </table-responsive>
@@ -27,9 +39,26 @@ import TableResponsive from '~/components/TableResponsive.vue'
 
 <style scoped>
 /* CSS */
-.button-64 {
+.bn5:active,
+.bn5 span {
+  background-color: rgb(5, 6, 45);
+  padding: 16px 24px;
+  border-radius: 6px;
+  width: 100%;
+  height: 100%;
+  transition: 300ms;
+}
+
+@media (min-width: 768px) {
+  .bn5 {
+    font-size: 24px;
+    min-width: 196px;
+  }
+}
+
+.bn5 {
   align-items: center;
-  background-image: linear-gradient(144deg, darkorchid, royalblue, mediumspringgreen);
+  background-image: linear-gradient(145deg, darkorchid, royalblue, mediumspringgreen);
   border: 0;
   border-radius: 8px;
   box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
@@ -49,31 +78,55 @@ import TableResponsive from '~/components/TableResponsive.vue'
   touch-action: manipulation;
   white-space: nowrap;
   cursor: pointer;
+  position: relative;
 }
 
-.button-64:active,
-.button-64:hover {
-  outline: 0;
-  box-shadow: 0px 0px 20px 0px mediumspringgreen;
+.bn5:hover {
+  z-index: 0;
 }
 
-.button-64 span {
-  background-color: rgb(5, 6, 45);
-  padding: 16px 24px;
-  border-radius: 6px;
+.bn5:hover:before {
+  content: "";
+  background: linear-gradient(
+    145deg,
+    darkorchid, royalblue, mediumspringgreen,
+    darkorchid, royalblue, mediumspringgreen,
+    darkorchid, royalblue, mediumspringgreen
+  );
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  background-size: 400%;
+  z-index: -1;
+  filter: blur(5px);
+  width: calc(100% + 4px);
+  height: calc(100% + 4px);
+  animation: glowingbn5 20s linear infinite;
+  opacity: 1;
+  transition: opacity 5s ease-in-out;
+  border-radius: 10px;
+}
+
+@keyframes glowingbn5 {
+  0% {
+    background-position: 0 0;
+  }
+  50% {
+    background-position: 400% 0;
+  }
+  100% {
+    background-position: 0 0;
+  }
+}
+.bn5:after {
+  z-index: -1;
+  content: "";
+  position: absolute;
   width: 100%;
   height: 100%;
-  transition: 300ms;
-}
-
-/* .button-64:hover span {
-  background: none;
-} */
-
-@media (min-width: 768px) {
-  .button-64 {
-    font-size: 24px;
-    min-width: 196px;
-  }
+  background: #191919;
+  left: 0;
+  top: 0;
+  border-radius: 10px;
 }
 </style>
