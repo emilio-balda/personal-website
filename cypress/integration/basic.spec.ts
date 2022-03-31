84a779c7e88b435a3500e91f76a5ddc7087a0579
Emilio Balda <me@emilio-balda.com>
2022-03-31 22:40:27 +0200

    test: fix end-to-end tests

--- cypress/integration/basic.spec.ts
+++ cypress/integration/basic.spec.ts
@@ -8,30 +8,12 @@ context('Basic', () => {
     cy.url()
       .should('eq', 'http://localhost:3333/')
 
-    cy.contains('[Home Layout]')
+    cy.contains('Emilio Balda')
       .should('exist')
 
-    cy.get('#input')
-      .type('Vitesse{Enter}')
-      .url()
-      .should('eq', 'http://localhost:3333/hi/Vitesse')
-
-    cy.contains('[Default Layout]')
-      .should('exist')
-
-    cy.get('[btn]')
+    cy.get('button span:first')
       .click()
       .url()
-      .should('eq', 'http://localhost:3333/')
-  })
-
-  it('markdown', () => {
-    cy.get('[title="About"]')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3333/about')
-
-    cy.get('pre.language-js')
-      .should('exist')
+      .should('eq', 'http://localhost:3333/contact')
   })
 })
