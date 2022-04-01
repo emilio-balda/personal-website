09645bdb250e94593d019e71b6486b062bf289de
Emilio Balda <me@emilio-balda.com>
2022-04-01 21:26:48 +0200

    chore: replace dummy pictures in portfolio for real ones

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -22,25 +22,25 @@ const content: Array<ContentItem> = [
   {
     title: 'Dataray Web App',
     description: 'Together with a team of 3 developers, we built a data science SaaS product called Dataray. The backend is built in Python, using FastAPI and DJango. The frontend is built in TypeScript, using Vue 3 and Tailwindcss. It is hosted in Google Cloud using Kubernetes and Helm. Tests are written with pytest and integration tests are implemented with GitLab CI.',
-    image: 'https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80',
+    image: 'https://images.pexels.com/photos/2832382/pexels-photo-2832382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     content_component: DatarayContent,
   },
   {
     title: 'This Website',
     description: 'This website is written 100% in TypeScript using Vue 3 and Tailwind CSS. The code is linted with ESLint. Unit tests are written with Jest, while Cypress handles integration tests.',
-    image: 'https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80',
+    image: 'https://images.pexels.com/photos/3374210/pexels-photo-3374210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     content_component: ServerlessWebsiteContent,
   },
   {
     title: 'Research Paper: Theoretical Result for Neural Networks',
     description: 'The goal was to publish a theoretical paper about the generalization capabilities of deep neural networks under the presence of a malicious attacker modifying the inputs of the network. I used tools from statistical learning theory to derive theoretical bounds that scaled better with the input dimension and the number of classes with respect to other existing bounds. The paper was published in AISTATS 2020.',
-    image: 'https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80',
     content_component: BoundsPaperContent,
+    image: 'https://images.pexels.com/photos/4021521/pexels-photo-4021521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   },
   {
     title: 'Research Paper: Adversarial Examples in Neural Networks',
     description: 'The goal was to publish a journal about new algorithms to generate "adversarial examples". I designed a mathematical framework that allowed me to derive new algorithms suited for different computer vision tasks. These algorithms outperformed their counterparts in the task of fooling neural networks. The paper was published in the renowned journal "IEEE Transactions on Signal Processing".',
-    image: 'https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80',
+    image: 'https://images.pexels.com/photos/3308588/pexels-photo-3308588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     content_component: AdversarialPaperContent,
   },
 ]
@@ -65,9 +65,11 @@ onMounted(() => {
             class="flex items-center justify-center w-80 h-[35rem] rounded-lg drop-shadow-xl bg-cover bg-center"
             :style="`background-image: url('${item.image}')`"
           >
-            <h1 class="text-2xl text-white font-bold px-2">
-              {{ item.title }}
-            </h1>
+            <div class="flex items-center justify-center h-[110px] w-full px-2 bg-black-900/50">
+              <h1 class="text-2xl text-white font-bold">
+                {{ item.title }}
+              </h1>
+            </div>
           </div>
         </template>
         <template #back>
